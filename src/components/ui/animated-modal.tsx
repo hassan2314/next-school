198f6036e1bf2919d2419a9068c3fae698196c94
Hassan Ahmed <hassan87691@gmail.com>
2025-08-03 13:26:09 +0500

    animated model without error

--- src/components/ui/animated-modal.tsx
+++ src/components/ui/animated-modal.tsx
@@ -220,8 +220,8 @@ const CloseIcon = () => {
 // Hook to detect clicks outside of a component.
 // Add it in a separate file, I've added here for simplicity
 export const useOutsideClick = (
-  ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  ref: React.RefObject<HTMLDivElement | null>,
+  callback: (event: MouseEvent | TouchEvent) => void
 ) => {
   useEffect(() => {
     const listener = (event: any) => {
